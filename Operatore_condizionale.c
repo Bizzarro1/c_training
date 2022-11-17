cd0a447f349c311a95a7beffb6006812245efb46
Bizzarro1 <117869411+Bizzarro1@users.noreply.github.com>
2022-11-17 23:37:16 +0100

    Update and rename Operatore_?.c to Operatore_condizionale.c

--- Operatore_condizionale.c
+++ Operatore_condizionale.c
@@ -1,6 +1,6 @@
 /******************************************************************************
 
-Operatore '?' 
+Operatore condizionale '?' 
 
 CONDIZIONE ? CONSEGUENZA (true) : ALTERNATIVA (false)
      
@@ -18,5 +18,26 @@ int main (void)
         ++count;
     } // fine di while
     return 0;
+} // fine del programma
+
+
+
+
+#include <stdio.h>
+
+int main (void)
+{
+    int row = 10;
+    
+    while (row >= 1) {
+        int column = 1;
+        
+        while (column <= 10) {
+            printf("%s", (row % 2) ? "<": ">");
+            ++column;
+        } 
+        --row;
+        puts("");
+    } // fine del while
 } // fine del programma
 
