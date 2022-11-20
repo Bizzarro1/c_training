a763def9cd06128f71bb450bd698ed002159fff9
Bizzarro1 <117869411+Bizzarro1@users.noreply.github.com>
2022-11-20 16:23:10 +0100

    Add files via upload

---
+++ ES-4.23.c
@@ -0,0 +1,146 @@
+/***********
+ 
+ ESERCIZIO 4.29 (Istruzioni PRINTF)
+
+
+
+#include <stdio.h>
+
+int main (void) {
+    
+    int i=2, j=2, k=3, m=2;
+    
+    printf("%d", i == 1 );
+ return 0;
+} // fine del programma
+
+#include <stdio.h>
+
+int main (void) {
+    
+    int i=2, j=2, k=3, m=2;
+    
+    printf("%d", j == 3);
+    return 0;
+} // fine del programma
+
+
+#include <stdio.h>
+
+int main (void) {
+     int i=2, j=2, k=3, m=2;
+     
+     printf("%d", i >= 1 && j < 4);
+    
+    return 0;
+} // fine del programma
+
+
+
+#include <stdio.h>
+
+int main (void) {
+    int i=2, j=2, k=3, m=2;
+    
+    printf("%d", m <= 99 && k < m);
+    
+    return 0;
+} // fine del programma
+
+#include <stdio.h>
+
+int main (void) {
+       int i=2, j=2, k=3, m=2;
+       
+       printf("%d", j >= i || k == m);
+    
+    return 0;
+} // fine del programma
+
+
+#include <stdio.h>
+
+int main (void) {
+       int i=2, j=2, k=3, m=2;
+       
+       printf("%d", k + m < j || 3 - j >= k);
+    
+    return 0;
+} // fine del programma
+
+
+
+#include <stdio.h>
+
+int main (void) {
+       int i=2, j=2, k=3, m=2;
+       
+       printf("%d", !m);
+    
+    return 0;
+} // fine del programma
+
+
+
+#include <stdio.h>
+
+int main (void) {
+       int i=2, j=2, k=3, m=2;
+       
+       printf("%d", !(j - m));
+    
+    return 0;
+} // fine del programma
+
+
+#include <stdio.h>
+
+int main (void) {
+       int i=2, j=2, k=3, m=2;
+       
+       printf("%d", !(k > m));
+    
+    return 0;
+} // fine del programma
+
+
+
+#include <stdio.h>
+
+int main (void) {
+       int i=2, j=2, k=3, m=2;
+       
+       printf("%d", !(j > k));
+    
+    return 0;
+} // fine del programma
+
+*************/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
